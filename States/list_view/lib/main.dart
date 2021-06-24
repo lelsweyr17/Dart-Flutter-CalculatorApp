34f3e2082455e23aa1138c4d8c77db2e79f6aa9b
Lippman Elsweyr <lelsweyr@va-c5.kzn.21-school.ru>
2021-06-24 14:24:09 +0300

    To Do List App

---
+++ States/list_view/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:list_view/pages/home.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.lime,
+  ),
+  home: HomePage(),
+));
