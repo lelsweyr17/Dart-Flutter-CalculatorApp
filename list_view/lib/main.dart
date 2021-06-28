d8c7c7c0c70377faa856c8d9d700d1fb97837b39
Lippman Elsweyr <lelsweyr@et-l4.kzn.21-school.ru>
2021-06-28 20:42:52 +0300

    Delete old version

--- list_view/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:list_view/pages/home.dart';
-
-void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primaryColor: Colors.lime,
-  ),
-  home: HomePage(),
-));
