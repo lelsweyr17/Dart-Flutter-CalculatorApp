f820faefb59f75745df1105047f2c9c16cbbbfbd
Lippman Elsweyr <lelsweyr@va-c5.kzn.21-school.ru>
2021-06-29 12:19:38 +0300

    Add interface elems

--- todolist/lib/pages/home.dart
+++ todolist/lib/pages/home.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+
+bool darkMode = false;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
     final snackBar = SnackBar(
         content: Text('Element is deleted'),
         action: SnackBarAction(
-            label: 'Cancel',
+            label: 'Undo',
             onPressed: () {
               setState(() {
                 _isVisible[index] = true;
@@ -33,126 +33,149 @@ class _HomePageState extends State<HomePage> {
     if (_isVisible[index] == false) toDoList.remove(index);
   }
 
+  Dismissible dismissibleCard(BuildContext context, int index) {
+    return Dismissible(
+      behavior: HitTestBehavior.translucent,
+      background: Container(
+        color: Colors.green[600],
+        width: 20.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Icon(Icons.archive, color: Colors.black54, size: 30)]),
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.red[600],
+        width: 20.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Icon(Icons.delete_forever, color: Colors.black54, size: 30)
+          ]),
+        ),
+      ),
+      movementDuration: const Duration(milliseconds: 500),
+      key: Key(toDoList[index]),
+      child: Column(
+        children: [
+          Visibility(
+            visible: _isVisible[index],
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10))),
+              child: ListTile(
+                title: Text(toDoList[index]),
+                leading: IconButton(
+                    icon: (_isDone[index] == false
+                        ? Icon(Icons.check_box_outline_blank_outlined)
+                        : Icon(Icons.check_box, color: Colors.green)),
+                    onPressed: () {
+                      setState(() {
+                        _isDone[index] = !_isDone[index];
+                      });
+                    }),
+                trailing: IconButton(
+                    icon: Icon(Icons.delete_outlined, color: Colors.red[600]),
+                    onPressed: () {
+                      confirmDelete(index);
+                    }),
+              ),
+            ),
+          ),
+        ],
+      ),
+      confirmDismiss: (DismissDirection direction) async {
+        if (direction == DismissDirection.startToEnd) {
+        } else {
+          confirmDelete(index);
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        child: new Container(
-          child: Center(
-              child: Text('TO DO',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
-          decoration: new BoxDecoration(
-              gradient: new LinearGradient(colors: [Colors.lime, Colors.teal]),
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 20.0,
-                  spreadRadius: 1.0,
-                )
-              ]),
-        ),
-        preferredSize: new Size(MediaQuery.of(context).size.width, 60.0),
-      ),
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text('TO DO',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20)),
+          backgroundColor: Colors.pink,
+          elevation: 15,
+          shadowColor: Colors.redAccent,
+          brightness: Brightness.dark,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)))),
       drawer: Drawer(
-        child: new ListView(
-          children: <Widget> [
-            new DrawerHeader(child: new Text('TO DO', style: TextStyle(fontWeight: FontWeight.bold)), padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0)),
-            new ListTile(
-              title: new Text('First Menu Item'),
-              onTap: () {},
-            ),
-            new ListTile(
-              title: new Text('Second Menu Item'),
-              onTap: () {},
-            ),
-            new Divider(),
-            new ListTile(
-              title: new Text('About'),
-              onTap: () {},
-            ),
-          ],
-        )
-      ),
+          child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('TO DO',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0)),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          darkMode = !darkMode;
+                        });
+                      },
+                      icon: (darkMode == false
+                          ? Icon(Icons.dark_mode_outlined)
+                          : Icon(Icons.light_mode))),
+                ],
+              ),
+              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0)),
+          ListTile(
+            title: Text('First Menu Item'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Second Menu Item'),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            title: Text('About'),
+            onTap: () {},
+          ),
+        ],
+      )),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            behavior: HitTestBehavior.translucent,
-            background: Container(
-              color: Colors.green[600],
-              width: 20.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(Icons.archive, color: Colors.black54, size: 30)
-                    ]),
-              ),
-            ),
-            secondaryBackground: Container(
-              color: Colors.red[600],
-              width: 20.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Icon(Icons.delete_forever, color: Colors.black54, size: 30)
-                ]),
-              ),
-            ),
-            movementDuration: const Duration(milliseconds: 500),
-            key: Key(toDoList[index]),
-            child: Column(
-              children: [
-                Visibility(
-                  visible: _isVisible[index],
-                  child: Card(
-                    child: ListTile(
-                      title: Text(toDoList[index]),
-                      leading: IconButton(
-                          icon: (_isDone[index] == false
-                              ? Icon(Icons.check_box_outline_blank_outlined)
-                              : Icon(Icons.check_box, color: Colors.green)),
-                          onPressed: () {
-                            setState(() {
-                              _isDone[index] = !_isDone[index];
-                            });
-                          }),
-                      trailing: IconButton(
-                          icon: Icon(Icons.delete_outlined,
-                              color: Colors.red[600]),
-                          onPressed: () {
-                            confirmDelete(index);
-                          }),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            confirmDismiss: (DismissDirection direction) async {
-              if (direction == DismissDirection.startToEnd) {
-              } else {
-                confirmDelete(index);
-              }
-            },
-          );
+          return dismissibleCard(context, index);
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Container(
           width: 60,
           height: 60,
-          child: Icon(Icons.add_box_outlined, size: 35, color: Colors.black),
+          child: Icon(Icons.add_box_outlined, size: 30, color: Colors.black),
           decoration: BoxDecoration(
               shape: BoxShape.circle,
               boxShadow: [
                 BoxShadow(
                     color: Colors.grey.withOpacity(0.5),
                     spreadRadius: 5,
-                    blurRadius: 7,
+                    blurRadius: 30,
                     offset: Offset(0, 3))
               ],
-              gradient: LinearGradient(colors: [Colors.lime, Colors.teal])),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.red])),
         ),
         onPressed: () {
           showDialog(
@@ -160,7 +183,13 @@ class _HomePageState extends State<HomePage> {
               context: context,
               builder: (BuildContext context) {
                 return AlertDialog(
-                  title: Text('Add new element',
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10))),
+                  title: Text('Create new element',
                       style: TextStyle(fontWeight: FontWeight.bold)),
                   content: TextField(onChanged: (String value) {
                     _userToDo = value;
@@ -169,7 +198,7 @@ class _HomePageState extends State<HomePage> {
                     ElevatedButton(
                         style: ButtonStyle(
                             backgroundColor:
-                                MaterialStateProperty.all(Colors.teal)),
+                                MaterialStateProperty.all(Colors.pink)),
                         onPressed: () {
                           setState(() {
                             toDoList.add(_userToDo);
@@ -177,6 +206,10 @@ class _HomePageState extends State<HomePage> {
                           _isVisible.add(true);
                           _isDone.add(false);
                           Navigator.of(context).pop();
+                          final snackBar = SnackBar(
+                              content: Text('Element is created'),
+                              duration: Duration(seconds: 2));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
                         },
                         child: Text('ADD'))
                   ],
